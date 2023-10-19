94b82d6866be87e41922081e7ae66a69b55e1232
sebaxtri <sebaxtri@gmail.com>
2023-10-19 14:47:40 -0500

    "acutualizada dependencias para correr front y back al mismo tiempo"

--- Backend/HummingWings/HummingWings/settings.py
+++ Backend/HummingWings/HummingWings/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'local_humming_wings',
-        'USER': 'juanse',
+        'USER': 'juanse_root',
         'PASSWORD': 'admin123',
         'HOST': 'localhost',
         'PORT': '5432',
