776a8f89824fb9f7d9ada6df2b8fa3a3da14bae4
IngSebastianGomez <ing.juanse.gomez@gmail.com>
2023-11-23 20:50:13 -0500

    arreglando conflictos de merge

--- Frontend/web-page-hummingwings/src/views/WarningWindow.vue
+++ Frontend/web-page-hummingwings/src/views/WarningWindow.vue
@@ -9,23 +9,32 @@
     </button>
 
     <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header" style="background-color: red;">
-            <h5 class="modal-title" id="exampleModalLabel">Aviso</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            <p>¿Estas seguro de que quieres eliminar este ___ ?</p>
-          </div>
-          <div class="modal-footer justify-content-evenly">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-            <button type="button" class="btn btn-primary" @click="redirigirAlInicio" data-bs-dismiss="modal">Eliminar</button>
-          </div>
-        </div>
+    
+
+
+    <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
       </div>
     </div>
+  </div>
+</div>
   </div>
 </template>
 
